a527cf489eb94a4169bb57b8552044a5c44ea1ca
vidagharavian <vgharavian@gmail.com>
2022-02-15 18:57:07 +0330

    add project read me

--- recomendation_system/content_based_recommendation/recomender.py
+++ recomendation_system/content_based_recommendation/recomender.py
@@ -15,7 +15,7 @@ def get_group_rating(all_users_rating,similarity):
         group:pd.DataFrame
         new_dict['anime_id'].append(name)
         x=group.apply(lambda x:similarity[similarity['user_id']]['similarity']*x['rating'],axis=1)
-        r=sum(x)
+        r=sum(x)/len(x)
         new_dict['rating']=r
     return pd.DataFrame(new_dict)
 
@@ -35,10 +35,13 @@ def get_taring_set(user_id,users=None):
             anime_ids.update(user_anime_rating)
             user_ratings.append(user_anime_rating)
         all_users_rating=pd.concat(user_ratings)
-        get_group_rating(all_users_rating,similarity)
+        all_rating=get_group_rating(all_users_rating,similarity)
     clf=RandomForestRegressor()
-    x_train=m[m.anime_id.isin(list(anime_ids))]
-    y=pd.merge(x_train,x,on='anime_id',how='outer')
+    x_train=m[m.anime_id.isin(x['anime_id'])]
+    y=pd.merge(x_train,x,on='anime_id',how='inner')
+    if users is not None:
+        na_y=y[y.rating_y.isna()]
+        na_y.append(all_rating)
     x_train.drop(columns="anime_id",inplace=True)
     clf.fit(x_train,y['rating_y'])
     x_test=m[~m['anime_id'].isin(x['anime_id'])]
